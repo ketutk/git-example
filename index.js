405544879324e42dc86d1eff86eadf2ae2756e5e
Tatang Romadhona <romadhonatatang@gmail.com>
2024-03-08 21:41:55 +0700

    master feature

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('test git');
+function penjumlahan(a, b) {
+    if (isNaN(a) || isNaN(b)) throw new Error('must be a number');
+    return a + b;
+}
+
+console.loc(penjumlahan(5 + 'a'));
+console.loc(penjumlahan(5 + 5));
