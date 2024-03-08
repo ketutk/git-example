7321fdb20b6025d19fb6616190aed3a5c5fd930d
ketutk <ketutkrisnakertajaya@gmail.com>
2024-03-08 21:46:38 +0700

    feat: add pengurangan function

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function penjumlahan(a, b) {
-    if (isNaN(a) || isNaN(b)) throw new Error('must be a number');
-    return a + b;
+  if (isNaN(a) || isNaN(b)) throw new Error("must be a number");
+  return a + b;
 }
 
-console.loc(penjumlahan(5 + 'a'));
-console.loc(penjumlahan(5 + 5));
+console.log(penjumlahan(5, 5));
+
+function pengurangan(a, b) {
+  if (isNaN(a) || isNaN(b)) throw new Error("must be a number");
+  return a + b;
+}
+
+console.log(pengurangan(5, 5));
